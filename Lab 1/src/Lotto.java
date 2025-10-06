c4e8ca53a400bdc1dba09dd1590a8342a4ca4a5a
Adam Jabłoński <adam.jab2004@gmail.com>
2025-10-06 18:16:08 +0200

    Second Part

---
+++ Lab 1/src/Lotto.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+public class Lotto {
+
+    public static void main(String[] args) {
+
+        HashSet set = new HashSet();
+
+        for(int i =0;i <6;i++){
+            boolean isUnique = false;
+//            System.out.println(randomNumber);
+            while(!isUnique){
+                int randomNumber = (int) (Math.random()*49)+1;
+                if(!set.contains(randomNumber)){
+                    set.add(randomNumber);
+                    isUnique = true;
+                }
+                else{
+                    System.out.println("powtorka");
+                }
+            }
+        }
+        System.out.println(set);
+    }
+}
