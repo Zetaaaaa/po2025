d397485ffd63ab0f34a7b7cb6a4e9934d1edb775
Adam Jabłoński <adam.jab2004@gmail.com>
2025-10-06 17:46:29 +0200

    First ever commit (and lab work)

---
+++ Lab 1/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        String witam = "12";
+
+        System.out.print("Enter a number: ");
+       int liczba = Integer.parseInt(input.next());
+//       int liczba2 = Integer.parseInt((args[0]));
+        System.out.println(" ");
+
+        for (int i = 1; i <= liczba; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("x");
+            }
+
+            System.out.println(" ");
+
+        }
+        System.out.print(" ");
+        //reversed
+        System.out.println("Reversed");
+        for (int i = liczba - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j =0; j<liczba-i; j++) {
+                System.out.print("x");
+            }
+            System.out.println(" ");
+        }
+
+
+
+    }
+}
