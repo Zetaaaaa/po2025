390eaf03560b79b9eb31be72acf01c74f6a08848
Adam Jabłoński <adam.jab2004@gmail.com>
2025-10-10 20:45:55 +0200

    Added changing scene handling

---
+++ Labs/JavaFX/src/main/java/com/example/javafx/SceneController.java
@@ -0,0 +1,15 @@
+package com.example.javafx;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+
+
+
+
+}
